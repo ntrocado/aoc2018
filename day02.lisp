609fa5a0776b89e1872e73116b514988785d51c2
Nuno Trocado <ntrocado@gmail.com>
2018-12-03 03:27:38 +0000

    Complete Day 2

--- day02.lisp
+++ day02.lisp
@@ -20,3 +20,23 @@
 	:counting c2 :into twos
 	:counting c3 :into threes
 	:finally (return (* twos threes))))
+
+;;; Part 2
+
+(defun compare-id (id1 id2)
+  (loop :for a :across id1
+	:for b :across id2
+	:when (char-equal a b)
+	  :collect a))
+
+(defun answer-2 ()
+  (loop :with data := (make-array 250
+				  :initial-contents (read-input "day02-input.txt"))
+	:for id1 :across data
+	:for i :from 0
+	:when (loop :for id2 :across (subseq data i)
+		    :for comp := (compare-id id1 id2)
+		    :when (and comp
+			       (= 25 (length comp)))
+		      :return comp)
+	:return :it))
