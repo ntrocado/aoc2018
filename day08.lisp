52b4591a59a2f31a1275e19d43eb30fbd630de1b
Nuno Trocado <ntrocado@gmail.com>
2018-12-19 00:14:40 +0000

    Complete day 8

--- day08.lisp
+++ day08.lisp
@@ -26,3 +26,26 @@
 
 (defun answer-1 ()
   (sum-metadata))
+
+;;; Part 2
+
+(setf *lst* (parse-input))
+
+(defun collect-data (n)
+  (loop :repeat n
+	:collect (pop *lst*)))
+
+(defun value-of-root-node ()
+  (let ((child (pop *lst*))
+	(metadata (pop *lst*)))
+    (if (zerop child)
+	(read-lst metadata)
+	(loop :for ch-node :from child :downto 0
+	      :if (zerop ch-node)
+		:return (loop :for entry :in (collect-data metadata)
+			      :when (nth (1- entry) r) :sum :it)
+	      :else
+		:collect (value-of-root-node) :into r))))
+
+(defun answer-2 ()
+  (value-of-root-node))
